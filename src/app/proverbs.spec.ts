e08d023e9ae4bb4ba64de09612ad723b536c7135
“mnubi” <rodger.kamau@student.moringaschool.com>
2021-10-10 12:06:57 +0300

    created class for quotes

---
+++ src/app/proverbs.spec.ts
@@ -0,0 +1,7 @@
+import { Proverbs } from './proverbs';
+
+describe('Proverbs', () => {
+  it('should create an instance', () => {
+    expect(new Proverbs()).toBeTruthy();
+  });
+});
---
+++ src/app/proverbs.ts
@@ -0,0 +1,3 @@
+export class Proverbs {
+    constructor
+}
