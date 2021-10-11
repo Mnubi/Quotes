7236a055f383f11455c38a2b30a0fc9f0ea7c31a
“mnubi” <rodger.kamau@student.moringaschool.com>
2021-10-11 19:11:38 +0300

    completed toggle effect

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -13,7 +13,11 @@ export class QuotesComponent implements OnInit {
       new Proverbs('Maya Angelou', 'Poet', '“I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.”', '13/08/2012'),
       new Proverbs('Lupita Nyongo', 'Actor', '"When I look down at this golden statue, may it remind me and every little child that no matter where you are from your dreams are valid."', '3/3/2014'),
       new Proverbs('Mother Teressa', 'Nun','“It is easy to love the people far away. It is not always easy to love those close to us. It is easier to give a cup of rice to relieve hunger than to relieve the loneliness and pain of someone unloved in our own home. Bring love into your home for this is where our love for each other must start.”', '27/04/1960' ),
-  ]
+    ]
+
+    showauthor(index) {
+      this.quotes[index].showauthor = !this.quotes[index].showauthor;
+    }
   
   constructor() { }
 
