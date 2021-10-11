9f7897d73522cdea1e9bb110798dfe2b51a53484
“mnubi” <rodger.kamau@student.moringaschool.com>
2021-10-11 15:31:05 +0300

    added toggle effect

--- src/app/author/author.component.ts
+++ src/app/author/author.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import { Proverbs } from '../proverbs';
 
 @Component({
   selector: 'app-author',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./author.component.css']
 })
 export class AuthorComponent implements OnInit {
+
+  @Input() proverbs: Proverbs;
 
   constructor() { }
 
--- src/app/proverbs.ts
+++ src/app/proverbs.ts
@@ -1,5 +1,6 @@
 export class Proverbs {
+    showauthor:boolean; 
     constructor(public name: string, public title: string, public saying: string, public date: string) {
-
+        this.showauthor = false;
     }
 }
